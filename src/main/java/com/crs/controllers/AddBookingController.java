8415776167b897a8598a57a8626e30133da43a17
Jjiro <egohappensoon@gmail.com>
2020-07-08 19:50:47 +0300

    Made some changes to booking, stylings,employee, customer and some others. Had to commit this now with some bugs still available...

---
+++ src/main/java/com/crs/controllers/AddBookingController.java
@@ -0,0 +1,52 @@
+package com.crs.controllers;
+
+import com.crs.controllers.dto.BookingDto;
+import com.crs.services.BookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/add-booking")
+@RequiredArgsConstructor
+public class AddBookingController {
+
+    private final BookingService bookingService;
+
+    @ModelAttribute("booking")
+    public BookingDto bookingDto() {
+        return new BookingDto();
+    }
+
+    @GetMapping
+    public String showBookingForm(@ModelAttribute("messageType") String messageType,
+                              @ModelAttribute("message") String message, Model model) {
+        return "booking/add-booking";
+    }
+
+    @PostMapping
+    public String createBooking(@ModelAttribute("booking") @Valid BookingDto bookingDto,
+                              BindingResult result, RedirectAttributes redirectAttributes) throws Exception {
+
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("booking", bookingDto);
+            redirectAttributes.addFlashAttribute("message", "Error in creating a booking!");
+            redirectAttributes.addFlashAttribute("messageType", "error");
+            return "booking/add-booking";
+        }
+
+        bookingService.createBooking(bookingDto);
+        redirectAttributes.addFlashAttribute("message", "Booking has been successfully created.");
+        redirectAttributes.addFlashAttribute("messageType", "success");
+        return "redirect:/booking";
+    }
+
+}
