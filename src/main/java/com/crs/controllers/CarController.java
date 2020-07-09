965933406944dc2ed52d5ec63d2447ddb36ac4f6
Alar Paluvee <alar260@hotmail.com>
2020-07-09 20:13:17 +0300

    Modify car works

--- src/main/java/com/crs/controllers/CarController.java
+++ src/main/java/com/crs/controllers/CarController.java
@@ -1,9 +1,11 @@
 package com.crs.controllers;
 
+import com.crs.controllers.dto.CarDto;
 import com.crs.models.Car;
 import com.crs.services.CarService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,6 +25,34 @@ public class CarController {
         modelAndView.addObject("cars", cars);
         return modelAndView;
     }
+
+    @GetMapping("/update/{id}")
+    public String updateCarForm(@PathVariable("id") Long carId, @ModelAttribute("messageType") String messageType,
+                                @ModelAttribute("message") String message, Model model) {
+        Car car = carService.getById(carId);
+        if (car == null) {
+            throw new IllegalArgumentException("Car with this ID not found!");
+        }
+        model.addAttribute("car", car);
+
+        return "car/car-update";
+    }
+
+    @PostMapping("/update/{id}")
+    public Object updateCar(@PathVariable("id") Long carId, Car car, Model model) throws Exception {
+        car.setId(carId);
+        boolean updateResult = carService.updateCar(car);
+        if (updateResult) {
+            model.addAttribute("message", "Car has been successfully updated.");
+            model.addAttribute("messageType", "success");
+            return showAllCars();
+        }
+        model.addAttribute("car", car);
+        model.addAttribute("message", "Error in updating car");
+        model.addAttribute("messageType", "error");
+        return "redirect:/car";
+    }
+
 
     /*
     @ModelAttribute("car")
