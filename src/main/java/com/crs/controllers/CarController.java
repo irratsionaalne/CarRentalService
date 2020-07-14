3d43e12db3e9f781cf3b7d771bfd071d9d2a0f6e
Priit Herja <priit218@gmail.com>
2020-07-14 19:06:32 +0300

    car controllers cleaned

--- src/main/java/com/crs/controllers/CarController.java
+++ src/main/java/com/crs/controllers/CarController.java
@@ -2,13 +2,15 @@ package com.crs.controllers;
 
 import com.crs.models.Car;
 import com.crs.services.CarService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,6 +19,10 @@ import java.util.UUID;
 public class CarController {
     @Autowired
     private CarService carService;
+    @ModelAttribute
+    public Car car() {
+        return new Car();
+    }
 
     @GetMapping
     public ModelAndView showAllCars() {
@@ -53,46 +59,26 @@ public class CarController {
         return "redirect:/car";
     }
 
-
-    /*
-    @ModelAttribute("car")
-    public CarDto carDto() {
-        return new CarDto();
-    }
     @GetMapping("/add-car")
-    public String addCarForm(Model model) {
+    public String showCarRegistrationForm(@ModelAttribute("messageType") String messageType,
+                              @ModelAttribute("message") String message) {
         return "car/add-car";
     }
-    @PostMapping("/add-car")
-    public Object addCar(@ModelAttribute("car") @Valid CarDto carDto) throws Exception {
-        Car car = carService.createCar(carDto);
-        if (car != null) {
-            return "redirect:/login";
-        }
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("message", "Error in creating a car!");
-        modelAndView.addObject("messageType", "error");
-        modelAndView.setViewName("car/add-car");
-        return modelAndView;
-    }
-    @GetMapping("/update")
-    public String updateCarForm(Model model) {
-        return "update-car";
-    }
-    @PutMapping("/update/{id}")
-    public Object updateCar(@PathVariable("id") UUID carId, Car car, Model model) throws Exception {
-        car.setId(carId);
-        boolean updateResult = carService.updateCar(car);
-        if (updateResult) {
-            model.addAttribute("message", "Car has been successfully updated.");
-            model.addAttribute("messageType", "success");
-            return showAllCars();
-        }
-        model.addAttribute("car", car);
-        model.addAttribute("message", "Error in updating car");
-        model.addAttribute("messageType", "error");
-        return updateCarForm(model);
-    }
-     */
 
+    @PostMapping("/add-car")
+    public String registerCar(@ModelAttribute("car") @Valid Car car,
+                              BindingResult result, RedirectAttributes redirectAttributes) throws Exception {
+
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("car", car);
+            redirectAttributes.addFlashAttribute("message", "Error in creating a car!");
+            redirectAttributes.addFlashAttribute("messageType", "error");
+            return "car/add-car";
+        }
+
+        carService.createCar(car);
+        redirectAttributes.addFlashAttribute("message", "Car has been successfully created.");
+        redirectAttributes.addFlashAttribute("messageType", "success");
+        return "redirect:/car";
+    }
 }
